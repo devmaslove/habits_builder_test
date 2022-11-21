6166fa75e2e78fbd2c4b62150a21d9cdb9c9ae21
devmaslove <63534992+devmaslove@users.noreply.github.com>
2022-11-22 00:11:58 +0300

    Экраны Introduction

---
+++ lib/screens/introduction_screen.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/material.dart';
+import 'package:wstore/wstore.dart';
+
+class IntroductionScreenStore extends WStore {
+  static const pagesCount = 4;
+  int currentPage = 0;
+
+  @override
+  IntroductionScreen get widget => super.widget as IntroductionScreen;
+
+  static IntroductionScreenStore of(BuildContext context) {
+    return WStoreWidget.store<IntroductionScreenStore>(context);
+  }
+}
+
+class IntroductionScreen extends WStoreWidget<IntroductionScreenStore> {
+  const IntroductionScreen({super.key});
+
+  @override
+  IntroductionScreenStore createWStore() => IntroductionScreenStore();
+
+  @override
+  Widget build(BuildContext context, IntroductionScreenStore store) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: IntroductionScreenContent(),
+      ),
+    );
+  }
+}
+
+class IntroductionScreenContent extends StatelessWidget {
+  const IntroductionScreenContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final store = IntroductionScreenStore.of(context);
+    return Column(
+      children: [
+        Expanded(
+          child: PageView(
+            onPageChanged: (index) =>
+                store.setStore(() => store.currentPage = index),
+            children: const [
+              IndroductionPage(
+                title: 'Welcome to Monumental habits',
+                asset: 'assets/images/Illustration1.png',
+              ),
+              IndroductionPage(
+                title: 'Create new habit easily',
+                asset: 'assets/images/Illustration2.png',
+              ),
+              IndroductionPage(
+                title: 'Keep track of your progress',
+                asset: 'assets/images/Illustration3.png',
+              ),
+              IndroductionPage(
+                title: 'Join a supportive community',
+                asset: 'assets/images/Illustration4.png',
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 60),
+        Row(
+          children: [
+            const SizedBox(width: 24),
+            const IndicatorButton(text: 'Skip'),
+            Expanded(
+              child: Center(
+                child: WStoreValueBuilder<IntroductionScreenStore, int>(
+                  watch: (store) => store.currentPage,
+                  builder: (context, page) {
+                    return IndicatorDots(
+                      count: IntroductionScreenStore.pagesCount,
+                      current: page,
+                    );
+                  },
+                ),
+              ),
+            ),
+            const IndicatorButton(text: 'Next'),
+            const SizedBox(width: 24),
+          ],
+        ),
+        const SizedBox(height: 80),
+      ],
+    );
+  }
+}
+
+class IndicatorDots extends StatelessWidget {
+  final int count;
+  final int current;
+
+  const IndicatorDots({
+    super.key,
+    required this.count,
+    required this.current,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        for (int i = 0; i < count; i++) IndicatorDot(selected: i == current),
+      ],
+    );
+  }
+}
+
+class IndicatorDot extends StatelessWidget {
+  final bool selected;
+
+  const IndicatorDot({
+    super.key,
+    required this.selected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 150),
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      height: selected ? 13 : 11,
+      width: selected ? 13 : 11,
+      decoration: BoxDecoration(
+        color: selected ? const Color(0xFF573353) : const Color(0xFFF9B566),
+        border: selected
+            ? Border.all(
+                color: const Color(0xFF573353).withOpacity(0.2),
+                width: 2,
+                strokeAlign: StrokeAlign.outside,
+              )
+            : null,
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+  }
+}
+
+class IndicatorButton extends StatelessWidget {
+  final String text;
+
+  const IndicatorButton({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Color(0xFF573353),
+          fontSize: 17,
+        ),
+      ),
+    );
+  }
+}
+
+class IndroductionPage extends StatelessWidget {
+  final String title;
+  final String asset;
+
+  const IndroductionPage({
+    super.key,
+    required this.title,
+    required this.asset,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 80),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          child: Text(
+            title.toUpperCase(),
+            style: const TextStyle(
+              fontFamily: 'Klasik',
+              color: Color(0xFF573353),
+              fontSize: 32,
+              height: 1,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Expanded(
+          child: Center(
+            child: Image.asset(
+              asset,
+              fit: BoxFit.scaleDown,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          child: Text.rich(
+            TextSpan(
+              text: 'We can '.toUpperCase(),
+              children: [
+                TextSpan(
+                  text: 'help you '.toUpperCase(),
+                  style: const TextStyle(
+                    color: Color(0xFFFC9D45),
+                  ),
+                ),
+                TextSpan(
+                  text: 'to be a better version of '.toUpperCase(),
+                ),
+                TextSpan(
+                  text: 'yourself.'.toUpperCase(),
+                  style: const TextStyle(
+                    color: Color(0xFFFC9D45),
+                  ),
+                ),
+              ],
+            ),
+            style: const TextStyle(
+              color: Color(0xFF573353),
+              fontSize: 17,
+              height: 1.41,
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    );
+  }
+}
