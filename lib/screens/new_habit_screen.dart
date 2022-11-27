cead0db741d37f96dbfadc7af5c4a2a70e6294e5
devmaslove <63534992+devmaslove@users.noreply.github.com>
2022-11-27 18:30:51 +0300

    Добавил кастомную летающую кнопку в дизайне

--- lib/screens/new_habit_screen.dart
+++ lib/screens/new_habit_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:habits_builder_test/widgets/primary_action_button.dart';
+import 'package:habits_builder_test/widgets/primary_app_bar.dart';
 import 'package:wstore/wstore.dart';
 
 class NewHabitScreenStore extends WStore {
@@ -18,6 +20,30 @@ class NewHabitScreen extends WStoreWidget<NewHabitScreenStore> {
 
   @override
   Widget build(BuildContext context, NewHabitScreenStore store) {
+    return Scaffold(
+      appBar: const PrimaryAppBar(
+        title: 'New Habbit',
+        showBack: true,
+      ),
+      body: const SafeArea(
+        child: NewHabitScreenContent(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: PrimaryActionButton(
+        icon: Image.asset(
+          'assets/images/check.png',
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class NewHabitScreenContent extends StatelessWidget {
+  const NewHabitScreenContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container();
   }
 }
---
+++ lib/widgets/primary_action_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:habits_builder_test/resources/app_colors.dart';
+
+class PrimaryActionButton extends StatelessWidget {
+  const PrimaryActionButton({
+    super.key,
+    required this.onPressed,
+    required this.icon,
+  });
+
+  final VoidCallback onPressed;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          width: 6,
+          color: AppColors.secondary2.withOpacity(0.2),
+          strokeAlign: StrokeAlign.outside,
+        ),
+      ),
+      margin: const EdgeInsets.only(bottom: 16),
+      child: Material(
+        shape: const CircleBorder(),
+        color: AppColors.secondary2,
+        clipBehavior: Clip.antiAlias,
+        elevation: 40.0,
+        child: IconButton(
+          padding: const EdgeInsets.all(16),
+          onPressed: onPressed,
+          iconSize: 20,
+          icon: icon,
+        ),
+      ),
+    );
+  }
+}
